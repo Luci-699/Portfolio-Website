e8b375ddca7aab04c66cf5c7d054815face56c09
Faizz <madmaxboi.98@gmail.com>
2026-05-27 17:15:34 +0530

    Fix: remove unused useState import causing Netlify TS6133 build error

--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren, useEffect, useState } from "react";
+import { PropsWithChildren, useEffect } from "react";
 import About from "./About";
 import Career from "./Career";
 import Contact from "./Contact";
